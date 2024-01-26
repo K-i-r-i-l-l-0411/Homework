9045944119da7022f3506710fbf36df1a7658e9a
K-i-r-i-l-l-0411 <145560789+K-i-r-i-l-l-0411@users.noreply.github.com>
2024-01-26 13:01:36 +0300

    update

--- дз №7/task5.py
+++ дз №7/task5.py
@@ -18,3 +18,25 @@ English
 3 - [Russian, English,Japenese]
 1 - [English]
 """
+
+
+def f():
+    students = int(input('Введите кол-во учеников: '))
+    all_languages = set()
+    any_languages = set()
+    for _ in range(students):
+       num_languages = int(input('Введите кол-во языков, которые знает ученик: '))
+       students_languages = set()
+       for _ in range(num_languages):
+           languages = input('Введите язык, который знает ученик: ')
+           students_languages.add(languages)
+       if not all_languages:
+           all_languages = students_languages.copy()
+       else:
+           # Обновления множества языков, которые знают все ученики
+           all_languages = all_languages.intersection(students_languages)
+        # Обновление множества языков, которые знает хотя бы один ученик
+       any_languages = any_languages.union(students_languages)
+    print(len(all_languages), '-', list(all_languages))
+    print(len(any_languages), '-', list(any_languages))
+f()
